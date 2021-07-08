c27baf2b4029973ef77cc1efb11fb26ba652f78f
Harsh Yadav <harsh2005bhilai@gmail.com>
2021-07-08 17:30:26 +0530

    Added Hello World

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+principle=float(input("enter principle amount: "))
+rate=float(input("enter rate percentage: "))
+time=int(input("enter time in years: "))#mat padhna
+si=(principle*rate*time)/100
+print ("your si is: ",si) #bhag yaha se
+print ("time: ",time)# this line prints time 
+
